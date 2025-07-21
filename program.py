2ee934799f06b08fac183b1e629f4d26785e5537
Dominic Dragone <domedra91@gmail.com>
2025-07-21 12:08:47 -0400

    created choice variable outside class
    setting up user flow to select correct options and get redirected if they enter invalid choice

--- program.py
+++ program.py
@@ -13,7 +13,10 @@ print()
 class Books:
     def __init__(self):
         name = input("Please enter your name \n")
-        print("Hello and welcome {0} to Let's Read, your top book recommendation software.  Below are list of options.  Please review to get started".format(name))
+        print("")
+        print("Hello and welcome {0} to Let's Read, your top fictional book recommendation software.".format(name))
+        print()
+        print("Below are list of options to choose from.  Enter '1', '2', '3', or '4' to get started")
         
 
     def display_options(self):
@@ -22,23 +25,40 @@ class Books:
         print("3. Recently Added")
         print("4. Add a book to the library")
         print()
-        choice = input("Enter choice: \n")
-        print("Thanks for selecting option #{0}!".format(choice))
-
+        return 
     
-    
-    def get_books_by_genre():
+    def get_books_by_genre(self, genre):
         pass
 
-    def top_rated_books_():
+    def top_rated_books_(self):
         pass
 
-    def get_recently_added():
+    def get_recently_added(self):
         pass
 
-    def add_book():
+    def add_book(self, user_recommendation):
         pass
         
 
 my_books = Books()
 my_books.display_options()
+choice = input()
+
+if choice == 1:
+    prit()
+    print("Thanks for selecting {0}!".format(choice))
+elif choice == 2:
+    print()
+    print("Thanks for selecting {0}!".format(choice))
+elif choice == 3:
+    print()
+    print("Thanks for selecting {0}!".format(choice))
+elif choice == 4:
+    print()
+    print("Thanks for selecting {0}!".format(choice))
+else:
+    print()
+    print("Invalid choice.  Please enter an appropriate option")
+    my_books.display_options()
+    print()
+    choice = input()
