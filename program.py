1f9d743f0be3cb8e19e2492066d8fdfb4d4dfa80
Dominic Dragone <domedra91@gmail.com>
2025-07-23 12:18:16 -0400

    completed while loop
    to restart and reuse menu options until loop breaks at option 6

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 #let's read!
-#introduction to software
+#software header
 
 print("\n" * 2)
 print("------------------------")
@@ -11,22 +11,27 @@ print("------------------------")
 print("\n" * 2)
 
 class Books:
+#fake database of 'books'
     inventory = [["Investing 101","DD","Business", "4", "7"],["How to make a tape dispenser","JD","Drama", "2", "6"],["iPhone","BD","Classical", "4", "7"],["Coins 101","CD","Literature", "5", "4"],["Microscrope","MTD","None", "1", "7"],["Whiteboard","DD","Cool", "2", "1"],["Tory","GD","Business", "3", "6"],["iMAc","HP","Classical", "3", "7"],["Cards","JL","Fun", "5", "4"],["Magic Mouse","TC","Sports", "5", "6"],["Case: Held Alive","PW","sports", "3", "7"],["Thermal Labels","SZ","Literature", "4", "7"],["Classical 101","TM","Science", "4", "7"],["Billiards","JS","Sports", "4", "2"],["Spongebob","DW","Comedy", "5", "7"],["Mixmaster","JP","Sports", "5", "7"]]
 
     def __init__(self):
         name = input("Please enter your name \n")
-        print("Hello and welcome {0} to 'Let's Read', your top fictional book recommendation software.\n".format(name))
-        print("Below are list of options to choose from.  Enter '1', '2', '3', or '4' to get started\n")
+        print("\nHello and welcome {0} to 'Let's Read', your top fictional book recommendation software.\n".format(name))
+
+#functional methods for program
         
     def display_options(self):
+        print("Main Menu")
+        print("----------------------------------------------------")
+        print("Enter '1', '2', '3', '4', '5', or '6' to get started\n")
         print("1. Books by Genre")
         print("2. Top Rated Books")
         print("3. Recently Added")
-        print("4. Add a book to the library")
-        print("5. By search")
-        print("6. Quit\n")
-        return             
-    
+        print("4. Add A Book")
+        print("5. By Keyword Search")
+        print("6. Quit")
+        print("----------------------------------------------------")    
+
     def get_books_by_genre(self, genre):
         genre_first = []
         genre_filtered = []
@@ -37,13 +42,13 @@ class Books:
             else:
                 continue
         if len(genre_filtered) == 0:
-            print("Sorry, we don't have any books to recommend for that genre at the moment")
+            print("Sorry, we don't have any books to recommend for that genre at the moment.")
         else:
             print(genre_filtered)
         return genre_filtered
 
     def top_rated_books(self):
-        print("Here's the top 10 rated books in our database")
+        print("Here's the top 10 rated books in our database:")
         rated_list = []
         for book in my_books.inventory:
             rated_list.append([book[3], book[0], book[1], book[2], book[4]])
@@ -63,14 +68,14 @@ class Books:
 
     def add_book(self, user_book):
         title, author, genre, rating, date = user_book.split(",")
-        my_books.inventory.append([title, author, genre, rating, date])
+        my_books.inventory.append([title.strip(), author.strip(), genre.strip(), rating.strip(), date.strip()])
         print("\nYour book has been successfully added.")
         return my_books.inventory     
 
     def by_search(self, string):
         search = []
         lower = []
-        lower_string = string.lower()
+        lower_string = string.lower().strip()
         for book in my_books.inventory:
             lower.append([book[0].lower(), book[1].lower(), book[2].lower(), book[3], book[4]])
         print("Printing results for: " + string + "\n")
@@ -84,46 +89,85 @@ class Books:
         else:
             print("Sorry no results found in the database.")
         return search     
+    
+#program starts
 
 my_books = Books()
 my_books.display_options()
 choice = int(input())
 
+#while loop to allow main menu to repeat until user quits and loop breaks there
 while True:
     if choice == 1:
-        print("Thanks for selecting {0}!\nWhat genre would you like to search by?\n".format(choice))
-        user_genre = input()
-        genre_titled = user_genre.title()
-        my_books.get_books_by_genre(genre_titled)
-        break
-      
+        decision = 'y'
+        while decision == 'y':
+            print("\nWhat genre would you like to search by?")
+            user_genre = input()
+            genre_titled = user_genre.title()
+            my_books.get_books_by_genre(genre_titled)
+            print("\nWould you like to repeat this option? Enter y for yes, n to go back to the main menu\n")
+            decision = input()
+            decision = decision.lower()
+        else:
+            decision = 'n'
+            my_books.display_options()
+            choice = int(input())     
+        
     elif choice == 2:
-        print("Thanks for selecting {0}!\n".format(choice))
-        my_books.top_rated_books()
-        break
+        decision = 'y'
+        while decision == 'y':
+            my_books.top_rated_books()
+            print("\nWould you like to repeat this option? Enter y for yes, n to go back to the main menu\n")
+            decision = input()
+            decision = decision.lower()
+        else:
+            decision = 'n'
+            my_books.display_options()
+            choice = int(input())
 
     elif choice == 3:
-        print("Thanks for selecting {0}!\n".format(choice))
-        my_books.get_recently_added()
-        break
+        decision = 'y'
+        while decision == 'y':
+            my_books.get_recently_added()
+            print("\nWould you like to repeat this option? Enter y for yes, n to go back to the main menu\n")
+            decision = input()
+            decision = decision.lower()
+        else:
+            decision = 'n'
+            my_books.display_options()
+            choice = int(input())   
         
     elif choice == 4:
-        print("Thanks for selecting {0}!\n".format(choice))
-        print("Please add book in the following order, separated by commas: title, author, genre, rating, today's month number")
-        user_book = input()
-        user_book_titled = user_book.title()
-        my_books.add_book(user_book_titled)
-        break
+        decision = 'y'
+        while decision == 'y':
+            print("Please add book in the following order, separated by commas: title, author, genre, rating, today's month number")
+            user_book = input()
+            user_book_titled = user_book.title()
+            my_books.add_book(user_book_titled)
+            print("\nWould you like to repeat this option? Enter y for yes, n to go back to the main menu\n")
+            decision = input()
+            decision = decision.lower()
+        else:
+            decision = 'n'
+            my_books.display_options()
+            choice = int(input())   
                  
     elif choice == 5:
-        print("Thanks for selecting {0}!\n".format(choice))
-        print("Please enter a keyword, title, genre, or author to search database:")
-        user_input = input()
-        my_books.by_search(user_input) 
-        break 
-
+        decision = 'y'
+        while decision == 'y':
+            print("Please enter a keyword, title, genre, or author to search database:")
+            user_input = input()
+            my_books.by_search(user_input) 
+            print("\nWould you like to repeat this option? Enter y for yes, n to go back to the main menu\n")
+            decision = input()
+            decision = decision.lower()
+        else:
+            decision = 'n'
+            my_books.display_options()
+            choice = int(input())    
+     
     elif choice == 6:
-        print("Have a nice day!\n".format(choice))
+        print("Have a nice day!\n")
         break
 
     else:
